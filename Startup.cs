a35d4071cdbfa3ea81c4bd7d7db0409c2847e372
Connor Clarkson <clarksoc@sheridancollege.ca>
2019-11-07 17:17:23 -0500

    The Initial Commit

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace FinancialEnterpriseGenie
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +26,10 @@ namespace FinancialEnterpriseGenie
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
