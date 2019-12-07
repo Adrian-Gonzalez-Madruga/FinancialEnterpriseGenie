1659e75916fb3ff6bc6154717801f0da1e4e6f8f
Adrian-Gonzalez-Madruga <35780225+Adrian-Gonzalez-Madruga@users.noreply.github.com>
2019-12-07 13:56:58 -0500

    Added Item and Sale Model

---
+++ Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialEnterpriseGenie.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public String ProductName { get; set; }
+        public double Price { get; set; }
+        public double Rating { get; set; }
+        public String Color { get; set; }
+    }
+}
---
+++ Models/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialEnterpriseGenie.Models
+{
+    public class Sale
+    {
+        public int ID { get; set; }
+        public Item Item { get; set; }
+        public int Units { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
