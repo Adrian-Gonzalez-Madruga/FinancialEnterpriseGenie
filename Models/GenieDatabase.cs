6e1fdae264959358e0539402cf0fbf1016dbb283
Connor Clarkson <clarksoc@sheridancollege.ca>
2019-12-07 14:20:55 -0500

    Dropped Database

--- Models/GenieDatabase.cs
+++ Models/GenieDatabase.cs
@@ -8,9 +8,17 @@ namespace FinancialEnterpriseGenie.Models
 {
     public class GenieDatabase : DbContext
     {
+        public DbSet<Credentials> Credentials { get; set; }
+        public DbSet<Distributor> Distributors { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<User> Users { get; set; }
+
 
         public GenieDatabase(DbContextOptions<GenieDatabase> options) : base(options)
         {
+
 
         }
 
