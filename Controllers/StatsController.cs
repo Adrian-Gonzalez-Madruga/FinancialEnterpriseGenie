46f84f780b4b9de355f2aea36964dbc93fa6a062
Adrian-Gonzalez-Madruga <35780225+Adrian-Gonzalez-Madruga@users.noreply.github.com>
2019-12-12 12:48:20 -0500

    added 2 more graph types

--- Controllers/StatsController.cs
+++ Controllers/StatsController.cs
@@ -11,7 +11,7 @@ namespace FinancialEnterpriseGenie.Controllers
     {
         private GenieDatabase _context;
         private Random random = new Random((int)DateTime.Now.Ticks);
-        private string[] graphTypeList = {"Sales Over Time"};
+        private string[] graphTypeList = { "Sales Over Time", "Profit Over Time", "Sales Growth Over Time" };
         public StatsController(GenieDatabase context)
         {
             _context = context;
@@ -43,7 +43,7 @@ namespace FinancialEnterpriseGenie.Controllers
                 List<Item> items1 = _context.Items.ToList();
                 List<Sale> sales1 = _context.Sales.ToList();
                 ViewBag.Items = items1;
-                ViewBag.Graph = DefaultGraph(items1, sales1.Min(s => s.Date), sales1.Max(s => s.Date), 1);
+                ViewBag.Graph = SalesByWeekGraph(items1, sales1.Min(s => s.Date), sales1.Max(s => s.Date), 1);
                 ViewBag.GraphTypeList = graphTypeList;
                 return View();
             }
@@ -61,7 +61,15 @@ namespace FinancialEnterpriseGenie.Controllers
 
             if (graphForm.Type == graphTypeList[0])
             {
-                ViewBag.Graph = DefaultGraph(selectedItems, graphForm.MinDate ?? sales.Min(s => s.Date), graphForm.MaxDate ?? sales.Max(s => s.Date), (int)(graphForm.NumWeeks ?? 1));
+                ViewBag.Graph = SalesByWeekGraph(selectedItems, graphForm.MinDate ?? sales.Min(s => s.Date), graphForm.MaxDate ?? sales.Max(s => s.Date), (int)(graphForm.NumWeeks ?? 1));
+            }
+            else if (graphForm.Type == graphTypeList[1])
+            {
+                ViewBag.Graph = ProfitByWeekGraph(selectedItems, graphForm.MinDate ?? sales.Min(s => s.Date), graphForm.MaxDate ?? sales.Max(s => s.Date), (int)(graphForm.NumWeeks ?? 1));
+            }
+            else if (graphForm.Type == graphTypeList[2])
+            {
+                ViewBag.Graph = SalesGrowthByWeekGraph(selectedItems, graphForm.MinDate ?? sales.Min(s => s.Date), graphForm.MaxDate ?? sales.Max(s => s.Date), (int)(graphForm.NumWeeks ?? 1));
             }
 
             ViewBag.Items = items;
@@ -78,7 +86,12 @@ namespace FinancialEnterpriseGenie.Controllers
             List<Item> items = _context.Items.ToList();
             List<Sale> sales = _context.Sales.ToList();
             ViewBag.Items = items;
-            ViewBag.Graph = DefaultGraph(items, sales.Min(s => s.Date), sales.Max(s => s.Date), 1);
+            if (sales.Count == 0)
+            {
+                ViewBag.Graph = new Graph() { name = "Item Sales Every  Week", shared = "true", xTitle = "Date", xValueFormatString = "DD MMM YYYY", yTitle = "Number Of Units Sold" };
+            } else { 
+                ViewBag.Graph = SalesByWeekGraph(items, sales.Min(s => s.Date), sales.Max(s => s.Date), 1);
+            }
             ViewBag.GraphTypeList = graphTypeList;
             return View();
         }
@@ -86,7 +99,7 @@ namespace FinancialEnterpriseGenie.Controllers
         public IActionResult Create()
         {
             //int[] startItemSale = {45, 30, 9, 90, 36, 42, 30, 24, 51, 81};
-            double[] weekFluctuation = {0.60, 0.70, 0.70, 0.85, 0.95, 1.25, 1.4, 1, 1.05, 1, 1.25, 1.2, 1.1, 0.9, 0.9, 0.8, 0.68, 0.95, 1.05, 1.05, 1.1, 1.1, 1, 1, 1, 1, 1.15, 1.1, 1.15, 1.2, 1.2, 1.25, 1.3, 1.3, 1.25, 1.3, 1.45, 1.2, 1, 1, 0.8, 1, 0.8, 0.65, 0.55, 0.5, 2, 2, 1, 1.25, 1.7, 1.9};
+            double[] weekFluctuation = { 0.60, 0.70, 0.70, 0.85, 0.95, 1.25, 1.4, 1, 1.05, 1, 1.25, 1.2, 1.1, 0.9, 0.9, 0.8, 0.68, 0.95, 1.05, 1.05, 1.1, 1.1, 1, 1, 1, 1, 1.15, 1.1, 1.15, 1.2, 1.2, 1.25, 1.3, 1.3, 1.25, 1.3, 1.45, 1.2, 1, 1, 0.8, 1, 0.8, 0.65, 0.55, 0.5, 2, 2, 1, 1.25, 1.7, 1.9 };
             int numOfYears = 10;
             int barSize = 12;
             List<int> sales = new List<int>();
@@ -105,9 +118,9 @@ namespace FinancialEnterpriseGenie.Controllers
                 for (int j = 0; j < (numOfYears * weekFluctuation.Length); j++)
                 {
                     double mean = sales.Count > 0 ? sales.Average() : startItemSale;
-                    double variance = (weekFluctuation[(j % weekFluctuation.Length)] * ((rating = item.Rating) > 3 ? 1 + ((rating - 3)/2) : ((rating - 1)/2)));
+                    double variance = (weekFluctuation[(j % weekFluctuation.Length)] * ((rating = item.Rating) > 3 ? 1 + ((rating - 3) / 2) : ((rating - 1) / 2)));
                     bool isPos = (variance >= 1);
-                    int min = Convert.ToInt32(-1*((barSize / 2) * (isPos ? (variance - 1) :(variance + 1))));
+                    int min = Convert.ToInt32(-1 * ((barSize / 2) * (isPos ? (variance - 1) : (variance + 1))));
                     int max = Convert.ToInt32(variance * (barSize / 2));
                     int unitsSold = Convert.ToInt32(Math.Round(pastSales.Average() + (random.Next(min, max))));
                     unitsSold = unitsSold < 0 ? 0 : unitsSold;
@@ -117,30 +130,30 @@ namespace FinancialEnterpriseGenie.Controllers
                     }
                     pastSales.Enqueue(unitsSold);
                     sales.Append(unitsSold);
-                    Sale sale = new Sale() {Item = item, Units = unitsSold, Date = date};
+                    Sale sale = new Sale() { Item = item, Units = unitsSold, Date = date };
                     _context.Sales.Add(sale);
                     date = date.AddDays(7);
                     counter++;
                 }
                 entryCounter.Append(counter);
-            } 
+            }
             _context.SaveChanges();
             String counterString = "";
             for (int i = 0; i < entryCounter.Count; i++)
             {
                 counterString += "\n" + i + ". " + entryCounter[i];
             }
-            return Content("ya ho" + counterString);
+            return RedirectToAction("Index", "Stats");
         }
 
         public IActionResult Delete()
         {
             _context.Sales.RemoveRange(_context.Sales.ToList());
             _context.SaveChangesAsync();
-            return Content("Deleted Sales");
+            return RedirectToAction("Index", "Stats");
         }
 
-        public Graph DefaultGraph(List<Item> items, DateTime minDate, DateTime maxDate, int numWeeks)
+        public Graph SalesByWeekGraph(List<Item> items, DateTime minDate, DateTime maxDate, int numWeeks)
         {
             Graph graph = new Graph() { name = "Item Sales Every " + ((numWeeks > 1) ? "" + numWeeks : "") + " Week" + ((numWeeks > 1) ? "s" : ""), shared = "true", xTitle = "Date", xValueFormatString = "DD MMM YYYY", yTitle = "Number Of Units Sold" };
             DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
@@ -148,14 +161,65 @@ namespace FinancialEnterpriseGenie.Controllers
             {
                 List<Sale> sales = _context.Sales.Where(s => s.Item == items[i] && s.Date >= minDate && s.Date <= maxDate).OrderBy(s => s.Date).ToList();
                 List<DataPoint> dataPoints = new List<DataPoint>();
-                for (int j = 0; j < sales.Count; j+=numWeeks)
+                for (int j = 0; j < sales.Count; j += numWeeks)
                 {
                     List<int> avgPointNumWeeks = new List<int>();
-                    for (int k = 0; (k < numWeeks && j+k < sales.Count); k++)
+                    for (int k = 0; (k < numWeeks && j + k < sales.Count); k++)
                     {
-                        avgPointNumWeeks.Add(sales[j+k].Units);
+                        avgPointNumWeeks.Add(sales[j + k].Units);
                     }
                     dataPoints.Add(new DataPoint((sales[j].Date - epoch).TotalSeconds * 1000, avgPointNumWeeks.Average()));
+                }
+                graph.elements.Add(new Element() { type = "spline", name = items[i].ProductName, dataPoints = dataPoints, xValueType = "dateTime", xValueFormatString = "DD MMM" });
+            }
+
+            return graph;
+        }
+
+        public Graph ProfitByWeekGraph(List<Item> items, DateTime minDate, DateTime maxDate, int numWeeks)
+        {
+            Graph graph = new Graph() { name = "Item Profit Every " + ((numWeeks > 1) ? "" + numWeeks : "") + " Week" + ((numWeeks > 1) ? "s" : ""), shared = "true", xTitle = "Date", xValueFormatString = "DD MMM YYYY", yTitle = "Profit in Dollars", yValueFormatString = "$###,###.##" };
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            for (int i = 0; i < items.Count; i++)
+            {
+                List<Sale> sales = _context.Sales.Where(s => s.Item == items[i] && s.Date >= minDate && s.Date <= maxDate).OrderBy(s => s.Date).ToList();
+                List<DataPoint> dataPoints = new List<DataPoint>();
+                for (int j = 0; j < sales.Count; j += numWeeks)
+                {
+                    List<double> sumPointNumWeeks = new List<double>();
+                    for (int k = 0; (k < numWeeks && j + k < sales.Count); k++)
+                    {
+                        sumPointNumWeeks.Add(((sales[j + k].Units * sales[j + k].Item.Price) - (sales[j + k].Units * sales[j + k].Item.MRP)));
+                    }
+                    dataPoints.Add(new DataPoint((sales[j].Date - epoch).TotalSeconds * 1000, sumPointNumWeeks.Sum()));
+                }
+                graph.elements.Add(new Element() { type = "spline", name = items[i].ProductName, dataPoints = dataPoints, xValueType = "dateTime", xValueFormatString = "DD MMM", yValueFormatString = "$###,###.##" });
+            }
+
+            return graph;
+        }
+
+        public Graph SalesGrowthByWeekGraph(List<Item> items, DateTime minDate, DateTime maxDate, int numWeeks)
+        {
+            Graph graph = new Graph() { name = "Item Sales Growth Every " + ((numWeeks > 1) ? "" + numWeeks : "") + " Week" + ((numWeeks > 1) ? "s" : ""), shared = "true", xTitle = "Date", xValueFormatString = "DD MMM YYYY", yTitle = "Sales Growth" };
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            int prevAmountSales = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                List<Sale> sales = _context.Sales.Where(s => s.Item == items[i] && s.Date >= minDate && s.Date <= maxDate).OrderBy(s => s.Date).ToList();
+                List<DataPoint> dataPoints = new List<DataPoint>();
+                for (int j = 0; j < sales.Count; j += numWeeks)
+                {
+                    List<int> avgPointNumWeeks = new List<int>();
+                    for (int k = 0; (k < numWeeks && j + k < sales.Count); k++)
+                    {
+                        avgPointNumWeeks.Add(sales[j + k].Units);
+                    }
+                    if (j != 0)
+                    {
+                        dataPoints.Add(new DataPoint((sales[j].Date - epoch).TotalSeconds * 1000, avgPointNumWeeks.Average() - prevAmountSales));
+                    }
+                    prevAmountSales = (int)avgPointNumWeeks.Average();
                 }
                 graph.elements.Add(new Element() { type = "spline", name = items[i].ProductName, dataPoints = dataPoints, xValueType = "dateTime", xValueFormatString = "DD MMM" });
             }
