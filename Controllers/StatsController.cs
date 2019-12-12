b6ca839473fd4f1c7260409dadf0f72ad23b3e35
Adrian-Gonzalez-Madruga <35780225+Adrian-Gonzalez-Madruga@users.noreply.github.com>
2019-12-12 11:48:29 -0500

    Stats Form fully operational with error handling

--- Controllers/StatsController.cs
+++ Controllers/StatsController.cs
@@ -19,8 +19,31 @@ namespace FinancialEnterpriseGenie.Controllers
         [HttpPost]
         public IActionResult Index(GraphForm graphForm)
         {
+            if (graphForm.MinDate == null || graphForm.MaxDate == null || graphForm.MaxDate < graphForm.MinDate || graphForm.MinDate > graphForm.MaxDate)
+            {
+                ModelState.AddModelError("MinDate", "MinDate must be before MaxDate");
+                ModelState.AddModelError("MaxDate", "MinDate must be before MaxDate");
+            }
+            if (graphForm.NumWeeks == null || graphForm.NumWeeks <= 0)
+            {
+                ModelState.AddModelError("NumWeeks", "Must specify how many weeks per DataPoint");
+            }
+            if (graphForm.ChosenItems.Count(c => c) == 0)
+            {
+                ModelState.AddModelError("ChosenItems", "Must Select at least 1 item");
+            }
+            if (!ModelState.IsValid)
+            {
+                List<Item> items1 = _context.Items.ToList();
+                List<Sale> sales1 = _context.Sales.ToList();
+                ViewBag.Items = items1;
+                ViewBag.Graph = DefaultGraph(items1, sales1.Min(s => s.Date), sales1.Max(s => s.Date), 1);
+                ViewBag.GraphTypeList = graphTypeList;
+                return View();
+            }
 
             List<Item> items = _context.Items.ToList();
+            List<Sale> sales = _context.Sales.ToList();
             List<Item> selectedItems = new List<Item>();
             for (int i = 0; i < items.Count; i++)
             {
@@ -29,18 +52,18 @@ namespace FinancialEnterpriseGenie.Controllers
                     selectedItems.Add(items[i]);
                 }
             }
-            ViewBag.Graph = DefaultGraph(selectedItems);
+            ViewBag.Graph = DefaultGraph(selectedItems, graphForm.MinDate ?? sales.Min(s => s.Date), graphForm.MaxDate ?? sales.Max(s => s.Date), (int)(graphForm.NumWeeks ?? 1));
 
             ViewBag.Items = items;
             ViewBag.GraphTypeList = graphTypeList;
             return View();
-            //return Content(" " + graphForm.ChosenItems[0] + " " + graphForm.ChosenItems[2] + " " + graphForm.ChosenItems[2] + " " + selectedItems.Count);
         }
         public IActionResult Index()
         {
             List<Item> items = _context.Items.ToList();
+            List<Sale> sales = _context.Sales.ToList();
             ViewBag.Items = items;
-            ViewBag.Graph = DefaultGraph(items);
+            ViewBag.Graph = DefaultGraph(items, sales.Min(s => s.Date), sales.Max(s => s.Date), 1);
             ViewBag.GraphTypeList = graphTypeList;
             return View();
         }
@@ -49,7 +72,7 @@ namespace FinancialEnterpriseGenie.Controllers
         {
             //int[] startItemSale = {45, 30, 9, 90, 36, 42, 30, 24, 51, 81};
             double[] weekFluctuation = {0.60, 0.70, 0.70, 0.85, 0.95, 1.25, 1.4, 1, 1.05, 1, 1.25, 1.2, 1.1, 0.9, 0.9, 0.8, 0.68, 0.95, 1.05, 1.05, 1.1, 1.1, 1, 1, 1, 1, 1.15, 1.1, 1.15, 1.2, 1.2, 1.25, 1.3, 1.3, 1.25, 1.3, 1.45, 1.2, 1, 1, 0.8, 1, 0.8, 0.65, 0.55, 0.5, 2, 2, 1, 1.25, 1.7, 1.9};
-            int numOfYears = 2;
+            int numOfYears = 10;
             int barSize = 12;
             List<int> sales = new List<int>();
             List<Item> items = _context.Items.ToList();
@@ -57,7 +80,7 @@ namespace FinancialEnterpriseGenie.Controllers
             List<int> entryCounter = new List<int>();
             for (int i = 0; i < items.Count; i++)
             {
-                DateTime date = new DateTime(2013, 01, 01);
+                DateTime date = new DateTime(2018, 01, 01);
                 Item item = items[i];
                 int counter = 0;
                 int numPreviousSales = 4;
@@ -102,17 +125,22 @@ namespace FinancialEnterpriseGenie.Controllers
             return Content("Deleted Sales");
         }
 
-        public Graph DefaultGraph(List<Item> items)
+        public Graph DefaultGraph(List<Item> items, DateTime minDate, DateTime maxDate, int numWeeks)
         {
-            Graph graph = new Graph() { name = "Item Sales By Week", shared = "true", xTitle = "Date", xValueFormatString = "DD MMM YYYY", yTitle = "Number Of Units Sold" };
+            Graph graph = new Graph() { name = "Item Sales Every " + ((numWeeks > 1) ? "" + numWeeks : "") + " Week" + ((numWeeks > 1) ? "s" : ""), shared = "true", xTitle = "Date", xValueFormatString = "DD MMM YYYY", yTitle = "Number Of Units Sold" };
             DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             for (int i = 0; i < items.Count; i++)
             {
-                List<Sale> sales = _context.Sales.Where(s => s.Item == items[i]).OrderBy(s => s.Date).ToList();
+                List<Sale> sales = _context.Sales.Where(s => s.Item == items[i] && s.Date >= minDate && s.Date <= maxDate).OrderBy(s => s.Date).ToList();
                 List<DataPoint> dataPoints = new List<DataPoint>();
-                for (int j = 0; j < sales.Count; j++)
+                for (int j = 0; j < sales.Count; j+=numWeeks)
                 {
-                    dataPoints.Add(new DataPoint((sales[j].Date - epoch).TotalSeconds * 1000, sales[j].Units));
+                    List<int> avgPointNumWeeks = new List<int>();
+                    for (int k = 0; (k < numWeeks && j+k < sales.Count); k++)
+                    {
+                        avgPointNumWeeks.Add(sales[j+k].Units);
+                    }
+                    dataPoints.Add(new DataPoint((sales[j].Date - epoch).TotalSeconds * 1000, avgPointNumWeeks.Average()));
                 }
                 graph.elements.Add(new Element() { type = "spline", name = items[i].ProductName, dataPoints = dataPoints, xValueType = "dateTime", xValueFormatString = "DD MMM" });
             }
--- Models/GraphForm.cs
+++ Models/GraphForm.cs
@@ -12,12 +12,12 @@ namespace FinancialEnterpriseGenie.Models
         [Required(ErrorMessage = "A Graph Type Must Be Selected")]
         public string Type { get; set; }
         [Required(ErrorMessage = "Number of Decimal Weeks Per DataPoint Mandatory")]
-        public decimal NumWeeks { get; set; }
+        public decimal? NumWeeks { get; set; }
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "Starting Date Required")]
-        public DateTime MinDate { get; set; }
+        public DateTime? MinDate { get; set; }
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "Ending Date Required")]
-        public DateTime MaxDate { get; set; }
+        public DateTime? MaxDate { get; set; }
     }
 }
